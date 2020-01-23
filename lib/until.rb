18f1d890bc9cd94d73bbfe5ac088756883abe398
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-01-23 17:07:25 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,7 +1,7 @@
 def using_until
   spell = 0
-  levitation_force = 6
-  until phrase == 10
+  #levitation_force = 6
+  until spell == 10
   #your code here
   puts "Wingardium Leviosa"
   spell += 1
